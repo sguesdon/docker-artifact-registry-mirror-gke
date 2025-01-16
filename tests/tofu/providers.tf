195e47cbe143bf06f81f4b7224597f1c3fa494c9
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2025-01-16 10:25:52 +0100

    bugfix: use valid url in kubernetes provider

--- tests/tofu/providers.tf
+++ tests/tofu/providers.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  host                   = data.google_container_cluster.this.endpoint
+  host                   = "https://${data.google_container_cluster.this.endpoint}"
   token                  = data.google_client_config.this.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.this.master_auth.0.cluster_ca_certificate)
 }
