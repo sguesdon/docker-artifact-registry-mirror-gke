2289f4e99aebc6994d5bca946bbc190aa39d6fa1
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2025-01-17 08:53:23 +0100

    bugfix: not override helm chart appVersion

--- resources/scripts/publish.sh
+++ resources/scripts/publish.sh
@@ -7,5 +7,5 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source "${SCRIPT_DIR}/common.sh"
 
 cp README.md "${HELM_DIR}/README.md"
-helm package "${HELM_DIR}" --version="${VERSION}" --app-version="${VERSION}"
+helm package "${HELM_DIR}" --version="${VERSION}"
 helm push "docker-artifact-registry-mirror-gke-${VERSION}.tgz" "${REPOSITORY}"
